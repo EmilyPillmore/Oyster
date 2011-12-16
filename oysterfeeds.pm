57b76b92156f895e9053e7c72c86e4d2c3ecddc9
Wyatt Pillmore <wpillmore@gmail.com>
2011-12-16 16:53:18 -0700

    registration and misc. utilities .pm for Oyster

---
+++ oysterfeeds.pm
@@ -0,0 +1,25 @@
+package oysterfeeds;
+use strict;
+use warnings;
+
+use Digest::MD5;
+use Algorithm::HowSimilar qw(compare);
+use Exporter 'import';
+
+our @EXPORT = qw(tolog register new_feed);
+
+
+sub tolog {
+	print shift ( ) . "\n";
+}
+
+sub register {
+	
+	return 0;
+	
+	
+}
+
+sub new_feed {
+	return 0;
+}

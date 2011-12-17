0df6259aeec4b3ab494b73c3719cb254084d93d1
Wyatt <wpillmore@gmail.com>
2011-12-16 21:59:00 -0700

    Update oysterfeeds.pm

--- oysterfeeds.pm
+++ oysterfeeds.pm
@@ -5,8 +5,9 @@ use warnings;
 use Digest::MD5;
 use Algorithm::HowSimilar qw(compare);
 use Exporter 'import';
+use IO::File;
 
-our @EXPORT = qw(tolog register new_feed);
+our @EXPORT = qw(tolog register new_feed get);
 
 
 sub tolog {
@@ -14,7 +15,6 @@ sub tolog {
 }
 
 sub register {
-
 	my $user = Digest::MD5::md5_hex($_[0]);
 	my $pass = Digest::MD5::md5_hex($_[1]);
 	qx(mkdir /home/emma/workspace/Oyster/oysterfeeds/$user);
@@ -22,6 +22,27 @@ sub register {
 		
 }
 
+sub get {
+	my $user = Digest::MD5::md5_hex($_[0]);
+	my $pass = Digest::MD5::md5_hex($_[1]);
+	my $io = IO::File->new("+>> /home/emma/workspace/Oyster/oysterfeeds/$user/$pass");
+	if(defined $io){
+		my @data = split('<feed></feed>', $io);
+		foreach(@data) {
+			if(defined $_){
+			return $_;
+			}
+		}
+	}
+	undef $io;
+}
 sub new_feed {
-	return 0;
+	my $user = Digest::MD5::md5_hex($_[0]);
+	my $pass = Digest::MD5::md5_hex($_[1]);
+	my $input = $_[2];
+	my $io = IO::File->new("+>> /home/emma/workspace/Oyster/oysterfeeds/$user/$pass");
+	if(defined $io){
+		print $io "<feed>$input</feed>";
+		}
+	undef $io;
 }

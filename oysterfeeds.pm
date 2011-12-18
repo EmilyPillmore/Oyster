2d534efaabd0ab0e69cdc8a0597c292e32393472
Wyatt <wpillmore@gmail.com>
2011-12-17 21:00:13 -0700

    Update oysterfeeds.pm

--- oysterfeeds.pm
+++ oysterfeeds.pm
@@ -15,34 +15,35 @@ sub tolog {
 }
 
 sub register {
-	my $user = Digest::MD5::md5_hex($_[0]);
-	my $pass = Digest::MD5::md5_hex($_[1]);
+	my $user = Digest::MD5::md5_hex($_[1]);
+	my $pass = Digest::MD5::md5_hex($_[2]);
 	qx(mkdir /home/emma/workspace/Oyster/oysterfeeds/$user);
 	qx(touch "/home/emma/workspace/Oyster/oysterfeeds/$user/$pass");
+	qx(chmod 666 "/home/emma/workspace/Oyster/oysterfeeds/$user/$pass");
 		
 }
 
 sub get {
-	my $user = Digest::MD5::md5_hex($_[0]);
-	my $pass = Digest::MD5::md5_hex($_[1]);
-	my $io = IO::File->new("+>> /home/emma/workspace/Oyster/oysterfeeds/$user/$pass");
-	if(defined $io){
+	my $user = Digest::MD5::md5_hex($_[1]);
+	my $pass = Digest::MD5::md5_hex($_[2]);
+	my $file = "/home/emma/workspace/Oyster/oysterfeeds/$user/$pass";
+	if(open(my $in, "<", $file){
 		my @data = split('<feed></feed>', $io);
 		foreach(@data) {
 			if(defined $_){
 			return $_;
 			}
 		}
+	close $in;
 	}
-	undef $io;
 }
 sub new_feed {
-	my $user = Digest::MD5::md5_hex($_[0]);
-	my $pass = Digest::MD5::md5_hex($_[1]);
-	my $input = $_[2];
-	my $io = IO::File->new("+>> /home/emma/workspace/Oyster/oysterfeeds/$user/$pass");
-	if(defined $io){
-		print $io "<feed>$input</feed>";
+	my $user = Digest::MD5::md5_hex($_[1]);
+	my $pass = Digest::MD5::md5_hex($_[2]);
+	my $input = $_[3];
+	my $file = "/home/emma/workspace/Oyster/oysterfeeds/$user/$pass";
+	if(open(my $in, "+>", $file){
+		print $in "<feed>$input</feed>";
 		}
-	undef $io;
+	close $io;
 }

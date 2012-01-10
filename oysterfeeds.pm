e86dace344d8380afd9faa6b62a315d40254d5e8
Wyatt <wpillmore@gmail.com>
2012-01-10 00:17:08 -0700

    Added some comments and cleaned up unnecessary subroutines.

--- oysterfeeds.pm
+++ oysterfeeds.pm
@@ -1,28 +1,28 @@
-package oysterfeeds;
+package oysterfeeds; #Do NOT use strict while using Fcntl and IO::file.
 use warnings;
 use Fcntl;
 use Digest::MD5;
-use Algorithm::HowSimilar qw(compare);
 use Exporter 'import';
 use IO::File;
 use Time::HiRes qw(setitimer getitimer);
 
-our @EXPORT = qw(tolog register new_feed feed_log ars_log bbc_log cnn_log 
-				slash_log hack_log toms_log npr_log get_logs timer);
-				
+our @EXPORT = qw(tolog register new_feed feed_log get_logs timer);
+ 
+#Array of log file absolute paths
 our @logs = ("/home/emma/workspace/Oyster/oysterfeeds/arslog",
-				"/home/emma/workspace/Oyster/oysterfeeds/cnnlog",
-				"/home/emma/workspace/Oyster/oysterfeeds/bbclog",
-				"/home/emma/workspace/Oyster/oysterfeeds/slashlog",
-				"/home/emma/workspace/Oyster/oysterfeeds/hacklog",
-				"/home/emma/workspace/Oyster/oysterfeeds/tomslog",
-				"/home/emma/workspace/Oyster/oysterfeeds/nprlog");
+				"/home/emma/workspace/Oyster/oysterfeeds/cnn.log",
+				"/home/emma/workspace/Oyster/oysterfeeds/bbc.log",
+				"/home/emma/workspace/Oyster/oysterfeeds/slash.log",
+				"/home/emma/workspace/Oyster/oysterfeeds/hack.log",
+				"/home/emma/workspace/Oyster/oysterfeeds/toms.log",
+				"/home/emma/workspace/Oyster/oysterfeeds/npr.log");
 
-
+#Simple console output subroutine
 sub tolog {
 	print shift ( ) . "\n";
 }
 
+#User Registration function
 sub register {
 	my $user = Digest::MD5::md5_hex($_[2]);
 	my $pass = Digest::MD5::md5_hex($_[3]);
@@ -34,7 +34,7 @@ sub register {
 		fi);
 }
 
-
+#User-specific feed parsing to be used in conjunction with registration function
 sub new_feed {
 	my $user = Digest::MD5::md5_hex($_[2]);
 	my $pass = Digest::MD5::md5_hex($_[3]);
@@ -43,89 +43,7 @@ sub new_feed {
 	
 }
 
-sub ars_log {
-	my $log = $_[1];
-	tolog($log);
-	qx(if [[ ! -f /home/emma/workspace/Oyster/oysterfeeds/arslog ]];
-		then 
-			touch /home/emma/workspace/Oyster/oysterfeeds/arslog;
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/arslog;
-		else
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/arslog;
-		fi);
-}
-
-sub bbc_log {
-	my $log = $_[1];
-	tolog($log);
-	qx(if [[ ! -f /home/emma/workspace/Oyster/oysterfeeds/bbclog ]];
-		then 
-			touch /home/emma/workspace/Oyster/oysterfeeds/bbclog;
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/bbclog;
-		else
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/bbclog;
-		fi);
-}
-
-sub cnn_log {
-	my $log = $_[1];
-	tolog($log);
-	qx(if [[ ! -f /home/emma/workspace/Oyster/oysterfeeds/cnnlog ]];
-		then 
-			touch /home/emma/workspace/Oyster/oysterfeeds/cnnlog;
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/cnnlog;
-		else
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/cnnlog;
-		fi);
-}
-
-sub slash_log {
-	my $log = $_[1];
-	tolog($log);
-	qx(if [[ ! -f /home/emma/workspace/Oyster/oysterfeeds/slashlog ]];
-		then 
-			touch /home/emma/workspace/Oyster/oysterfeeds/slashlog;
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/slashlog;
-		else
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/slashlog;
-		fi);
-}
-
-sub hack_log {
-	my $log = $_[1];
-	tolog($log);
-	qx(if [[ ! -f /home/emma/workspace/Oyster/oysterfeeds/hacklog ]];
-		then 
-			touch /home/emma/workspace/Oyster/oysterfeeds/hacklog;
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/hacklog;
-		else
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/hacklog;
-		fi);
-}
-
-sub toms_log {
-	my $log = $_[1];
-	tolog($log);
-	qx(if [[ ! -f /home/emma/workspace/Oyster/oysterfeeds/tomslog ]];
-		then 
-			touch /home/emma/workspace/Oyster/oysterfeeds/tomslog;
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/tomslog;
-		else
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/tomslog;
-		fi);
-}
-
-sub npr_log {
-	my $log = $_[1];
-	tolog($log);
-	qx(if [[ ! -f /home/emma/workspace/Oyster/oysterfeeds/nprlog ]];
-		then 
-			touch /home/emma/workspace/Oyster/oysterfeeds/nprlog;
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/nprlog;
-		else
-			echo '$log ' >> /home/emma/workspace/Oyster/oysterfeeds/nprlog;
-		fi);
-}
+#parse log files and return an array of most recent titles for XML feeds
 sub get_logs {
 	my @lines;
 	foreach(@logs){
@@ -140,6 +58,7 @@ sub get_logs {
 	return @lines;
 }
 
+#Add most recent title to log for that feed for update function
 sub feed_log(\$\$) {
 		qx(if [[ ! -f /home/emma/workspace/Oyster/oysterfeeds/$_[1].log ]];
 		then 
